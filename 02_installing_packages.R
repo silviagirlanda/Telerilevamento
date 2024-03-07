c11118e0eebf9b157ba5918fb4cb54c64f1fbba1
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2024-03-07 17:49:40 +0100

    Create 02_installing_packages.R

---
+++ 02_installing_packages.R
@@ -0,0 +1,8 @@
+#Installiamo pacchetto "Terra" con la funzione install.packages
+install.packages("terra") #mi raccomando ricordati le virgolette
+
+#per verificare che il pacchetto sia stato installato deve essere richiamato com la funzione library
+library("terra") #visualizzere la versione del pacchetto installato, qui: terra 1.7.71
+
+#su R abbiamo due errori: warning(bone fa lo stesso) o error(è un problema)
+#per rimuovere pacchetti: remove.packages()
