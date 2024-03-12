180a6d25de91606a1e54740404b420b592cec09f
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2024-03-12 16:38:05 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -6,3 +6,7 @@ library("terra") #visualizzere la versione del pacchetto installato, qui: terra 
 
 #su R abbiamo due errori: warning(bone fa lo stesso) o error(è un problema)
 #per rimuovere pacchetti: remove.packages()
+
+#Abbiamo 2 metodi per installare pacchetti: 
+# - cran (pacchetti controllati da sviuluppatori di R)> install.packages()
+# - pacchetti esterni da CRAN, come "imagery" che si trova su GitHub > install.github MA per usare questa dobbiamo usare un'altra funzione, che è devtools(che è su CRAN)
