5f8c6354d8f1da43b195b24dbadd569ca8da60fa
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2024-03-12 17:31:53 +0100

    02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -2,11 +2,22 @@
 install.packages("terra") #mi raccomando ricordati le virgolette
 
 #per verificare che il pacchetto sia stato installato deve essere richiamato com la funzione library
-library("terra") #visualizzere la versione del pacchetto installato, qui: terra 1.7.71
+library(terra) #visualizzere la versione del pacchetto installato, qui: terra 1.7.71
 
 #su R abbiamo due errori: warning(bone fa lo stesso) o error(è un problema)
 #per rimuovere pacchetti: remove.packages()
 
 #Abbiamo 2 metodi per installare pacchetti: 
 # - cran (pacchetti controllati da sviuluppatori di R)> install.packages()
-# - pacchetti esterni da CRAN, come "imagery" che si trova su GitHub > install.github MA per usare questa dobbiamo usare un'altra funzione, che è devtools(che è su CRAN)
+# - pacchetti esterni da CRAN, come "imagery" che si trova su GitHub > install_github MA per usare questa dobbiamo usare un'altra funzione, che è devtools(che è su CRAN)
+
+install.packages("devtools") #ricordati sempre le virgolette
+#dev.tools::install_github
+library(devtools) #or require()
+
+
+#installiamo la repository imageRy
+install_github("ducciorocchini/imageRy")
+
+#per verificare che l'installazione sia andata a buon fine
+library(imageRy) 
