cbc6d09776821a0500629e8adad63ed416aa2083
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2024-03-14 22:41:37 +0100

    Update 02_installing_packages.R

--- 02_installing_packages.R
+++ 02_installing_packages.R
@@ -1,23 +1,21 @@
-#Installiamo pacchetto "Terra" con la funzione install.packages
-install.packages("terra") #mi raccomando ricordati le virgolette
-
-#per verificare che il pacchetto sia stato installato deve essere richiamato com la funzione library
-library(terra) #visualizzere la versione del pacchetto installato, qui: terra 1.7.71
-
-#su R abbiamo due errori: warning(bone fa lo stesso) o error(è un problema)
-#per rimuovere pacchetti: remove.packages()
-
 #Abbiamo 2 metodi per installare pacchetti: 
-# - cran (pacchetti controllati da sviuluppatori di R)> install.packages()
-# - pacchetti esterni da CRAN, come "imagery" che si trova su GitHub > install_github MA per usare questa dobbiamo usare un'altra funzione, che è devtools(che è su CRAN)
+# - pacchetti da CRAN ( controllati da sviluppatori di R)> install.packages()
+# - pacchetti esterni dal CRAN, come "imageRy" che si trova su GitHub > install_github MA per usare questa dobbiamo usare un'altra funzione, che è devtools(che è su CRAN)
 
+#Installa pacchetto "Terra" con la funzione install.packages
+install.packages("terra") #ricorda le virgolette
+#Per verificare che il pacchetto sia stato installato, richiamalo con la funzione library
+library(terra) #visualizzerai la versione del pacchetto installato, qui: terra 1.7.71
+
+#Installa la funzione devtools dal CRAN per poi poter usare imageRy
 install.packages("devtools") #ricordati sempre le virgolette
-#dev.tools::install_github
 library(devtools) #or require()
-
-
-#installiamo la repository imageRy
+#Installa la repository imageRy da github
 install_github("ducciorocchini/imageRy")
-
-#per verificare che l'installazione sia andata a buon fine
+#Per verificare che il pacchetto sia stato installato, richiamalo con la funzione library
 library(imageRy) 
+
+#RIASSUMENDO  devtools::install_github  
+
+#Per rimuovere pacchetti: 
+remove.packages()
