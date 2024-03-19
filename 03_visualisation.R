a0ab8cbecfff6a5b2b3a0f97e4db09ea70cd6880
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2024-03-19 17:11:57 +0100

    Update 03_visualisation.R

--- 03_visualisation.R
+++ 03_visualisation.R
@@ -46,7 +46,7 @@ plot(b4,col=clch)
 b8<-im.import("sentinel.dolomites.b8.tif") #infrarosso
 plot(b8,col=clch)
 
-#Creiamo un multiframe con la seguente funzione:
+#Creiamo un MULTIFRAME con la seguente funzione:
 par(mf) #mf sta per multiframe
 par(mfrow=c(2,2)) #il primo valore indica il numero di righe, il secondo il numero di colonne. Sono un array quindi vanno concatenati con c
 #è stato così creato un telaio in cui andranno inserite le bande.
@@ -63,12 +63,35 @@ plot(b3,col=clch)
 plot(b4,col=clch)
 plot(b8,col=clch)
 
-#Impiliamo le bande tutte insieme, per creare un'immagine satellitare vera e propria.
+#Creiamo uno stack tramite la funzione c() per creare un'immagine satellitare MULTISPETTRALE, nominandolo a tuo piacimento.
 stacksent<-c(b2,b3,b4,b8) #creo un array con le diverse bande e lo assegno all'oggetto stacksent
-plot(stacksent,col=clch)
+plot(stacksent,col=clch)#Creiamo il plot con le 4 bande
 
 #Vogliamo plottare solo la banda 8. In R i singoli elementi si selezionano con le [].
 plot(stacksent[[4]],col=clch) #per lavorare su un singolo elemento di un vettore. Le quadre sono doppie perchè abbiamo una matrice.
 
-#Per distruggere il device precedente:
+#Per distruggere il device precedente(di fatto chiude il grafico e basta):
 dev.off()
+
+#19 MARZO 2023------------------------------------------------------------
+#RGB PLOTTING
+#Questo stacksent è uguale all'elemento n.2 che corrisponde al blu e via via così
+
+#stacksent[[1]] = b2 = blue
+#stacksent[[2]] = b3 = green
+#stacksent[[3]] = b4 = red
+#stacksent[[4]] = b8 = nir infrared
+
+im.plotRGB()
+#Dichiaro nome immagine(qui stacksent) e dichiaro quali sono le tre compnenti che corrispondono ai 3 filtri
+im.plotRGB(stacksent,r=3,g=2,b=1)
+im.plotRGB(stacksent,3,2,1)
+#Associamo il livello 1 al blu, il 2 al verde, il 3 al rosso -> otterrò un'immagine con i colori reali
+
+#Per evidenziare il verde, sostituiamo il filtro rosso con la componente infrarossa: tutto quello che riflette infrarosso diventerà rosso
+im.plotRGB(stacksent,4,2,1)
+
+#Esercizio: fai par con due immagini una a lato dell'altra, una con i colori reali, l'altra con quelli infrarosso
+par(mfrow=c(1,2))
+im.plotRGB(stacksent,3,2,1)
+im.plotRGB(stacksent,4,2,1)
