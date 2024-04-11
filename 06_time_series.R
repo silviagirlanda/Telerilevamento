f6120748692d91fb232169da2eae8e6ac7fdead9
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2024-04-11 17:03:49 +0200

    Update 06_time_series.R

--- 06_time_series.R
+++ 06_time_series.R
@@ -1,5 +1,5 @@
 #--------------------11 APRILE 2024
-#Secondo metodo di classficare i cambiamenti nel tempo (SCRIVITI APPUNTI DI ANIS DA IMMMAGINE) 
+#Secondo metodo di classficare i cambiamenti nel tempo
 
 #Riprendiamo i precedenti pacchetti:
 library(terra)
@@ -8,18 +8,18 @@ library(imageRy)
 #Richiamiamo la lista dei dati:
 im.list()
 
+#Importiamo le immagini EN (European Nitrogen) di gennaio e marzo 2020 e attribuiamole a degli oggetti:
 EN01<-im.import("EN_01.png") 
 EN13<-im.import("EN_13.png")
 
-par(mfrow=c(1,2))
-im.plotRGB.auto(EN01) #
-im.plotRGB.auto(EN13)
-par(mfrow=c(2,1))
+#Creiamo un multiframe, con le immagini una sopra all'altra:
+par(mfrow=c(2,1)) #2 righe e 1 colonna
 im.plotRGB.auto(EN01) #nuova funzione che prende in automatico le prime 3 bande (vedi manuale di imageRy)
 im.plotRGB.auto(EN13)
 
+
 difEN = EN01[[1]] - EN13[[1]] #Sottrazione tra pixel del primo livello di EN01 e pixel del primo livello di EN13. C'è l'uguale perchè si tratta di un'operazione matematica.
-cl <- colorRampPalette(c("blue","white","red")) (100) #blu è valore minimo, rosso è valore massimo
+cl <- colorRampPalette(c("blue","white","red")) (100) #scelgo la banda di colori: blu è valore minimo, rosso è valore massimo
 
 #Plottiamo:
 plot(difEN, col=cl)
