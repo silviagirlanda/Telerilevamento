d810262daf3f7d99bc8dac361e24493482455f41
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2024-04-23 16:27:13 +0200

    Create 07_importing_data.R

---
+++ 07_importing_data.R
@@ -0,0 +1,9 @@
+###23 aprile 2024
+# Importing data from external sources
+
+#Riprendiamo i seguenti pacchetti:
+library(terra)
+library(imageRy)
+
+#Con la seguente funzione, spieghiamo al sistema qual è la cartella del nostro pc che useremo per caricare i dati, indicandola tra le parentesi:
+setwd("/Users/silviagirlanda/Downloads")
