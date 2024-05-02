1c56aafb249e894304a918265ed2f0c2e972b3e8
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2024-05-02 17:04:36 +0200

    Update 09_multivariate_analysis.R

--- 09_multivariate_analysis.R
+++ 09_multivariate_analysis.R
@@ -5,3 +5,20 @@
 library(terra)
 library(imageRy)
 library(viridis)
+
+#Apriamo la lista di dati e usiamo le immagini delle dolomiti:
+im.list()
+
+#Importiamo i dati:
+b2 <- im.import("sentinel.dolomites.b2.tif") #blu
+b3 <- im.import("sentinel.dolomites.b3.tif") #verde
+b4 <- im.import("sentinel.dolomites.b4.tif") #rosso
+b8 <- im.import("sentinel.dolomites.b8.tif") #nir
+
+#Creiamo uno stack, unendo le varie immagini:
+sentdo<-c(b2, b3, b4, b8)
+
+#Creiamo un plot:
+im.plotRGB(sentdo, r=4, g=3, b=2) 
+#banda 4 montata su rosso, banda 3 su verde, banda 2 su blu
+
