e27d5dcc6a1a13d1d27492e8bc0df5bcbdba0709
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2025-01-27 17:58:57 +0100

    Update 06_time_series.R

--- 06_time_series.R
+++ 06_time_series.R
@@ -1,5 +1,5 @@
-#--------------------11 APRILE 2024
-###Secondo metodo di classficare i cambiamenti nel tempo
+#---------------------------------11 APRILE 2024
+###Secondo metodo di classficare i cambiamenti nel tempo, prendendo immagini in periodi diversi per vedere il cambiamento:
 
 #Riprendiamo i precedenti pacchetti:
 library(terra)
@@ -17,7 +17,7 @@ par(mfrow=c(2,1)) #2 righe e 1 colonna
 im.plotRGB.auto(EN01) #nuova funzione che prende in automatico le prime 3 bande (vedi manuale di imageRy)
 im.plotRGB.auto(EN13)
 
-
+# Facendo la differenza tra due bande si riesce a quantificiare la magnitudo del cambiamento tra la prima banda e la seconda:
 difEN = EN01[[1]] - EN13[[1]] #Sottrazione tra pixel del primo livello di EN01 e pixel del primo livello di EN13. C'è l'uguale perchè si tratta di un'operazione matematica.
 cl <- colorRampPalette(c("blue","white","red")) (100) #scelgo la banda di colori: blu è valore minimo, rosso è valore massimo
 
