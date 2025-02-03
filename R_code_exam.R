2bfbb6a6014668d4e520205aec18148ff32522fe
silviagirlanda <162352408+silviagirlanda@users.noreply.github.com>
2025-02-03 17:07:58 +0100

    Update R_code_exam.R

--- R_code_exam.R
+++ R_code_exam.R
@@ -32,13 +32,6 @@ setwd("C:/Telerilevamento") #posizionata vicino alla sorgente del computer per f
 
 # Per ogni anno d'analisi, creo uno stack comprendente tutte le bande, assegnate precedentemente ad un oggetto. 
 
-#Alla fine ho cambiato immagine perchè quezta mi dava problemi con NDVI classificazione:
-#a17_4 <- rast("17_4.tiff") #red
-#a17_3 <- rast("17_3.tiff") #green
-#a17_2 <- rast("17_2.tiff") #blue
-a#17_8 <- rast("17_8.tiff") #nir
-#a17 <- c(a17_4, a17_3, a17_2, a17_8)
-
 a17_4 <- rast("17b_4.tiff") #red
 a17_3 <- rast("17b_3.tiff") #green
 a17_2 <- rast("17b_2.tiff") #blue
@@ -69,7 +62,13 @@ par(mfrow=c(1,3))
 im.plotRGB(a17, 4,3,2,title="2017 (nir)")
 im.plotRGB(a19, 4,3,2,title="2019 (nir)")
 im.plotRGB(a24, 4,3,2,title="2024 (nir)")
-##############àQUI SI METTONO IN ORDINE COSì PERCHè SI FA COSì E BOONE?
+##############QUI SI METTONO IN ORDINE COSì PERCHè SI FA COSì E BOONE?
+
+#Faccio  la differenza tra il nir del 2017 e quello del 2024 per vedere la differenza in temrini di salute della vegetazione:
+difnir <- a17 [[4]] - a24[[4]] 
+# Lo plotto: 
+cdiff<- colorRampPalette(c("red","white","darkslategray")) (100) #valori rossi dove la componente boschiva è cambiata
+plot(difnir,col=cdiff) 
 
 #Multiframe confronto tra tc e nir
 par(mfrow=c(2,3))
@@ -80,22 +79,8 @@ im.plotRGB(a17, 4,3,2,title="2017 (nir)")
 im.plotRGB(a19, 4,3,2,title="2019 (nir)")
 im.plotRGB(a24, 4,3,2,title="2024 (nir)")
 
-
 #Chiudo il device precedente:
 dev.off()
-
-##### DVI ###### ELIMINALOOOOOOOOOOOOOO POIIIIIIII
-# Calcolo della DVI
-cl<-colorRampPalette(viridis(7))(100)
-no<-colorRampPalette(c("darkblue","yellow","red","black"))(100)
-DVI_2017<-a17[[4]]-a17[[1]]
-plot(DVI_2017,col=cl)
-
-DVI_2019<-a19[[4]]-a19[[1]]
-plot(DVI_2019,col=cl)
-
-DVI_2024<-a24[[4]]-a24[[1]]
-plot(DVI_2024,col=cl)
 
 ##### NDVI #####
 # Calcolo l'NDVI (Normalized Difference Vegetation Index), utilizzato se le immagini hanno bit differenti ed è quindi necessaria una normalizzazione.
@@ -105,7 +90,7 @@ plot(DVI_2024,col=cl)
 cl<-colorRampPalette(viridis(7))(100)
 
 NDVI_2017<-(a17[[4]]-a17[[1]])/(a17[[4]]+a17[[1]])
-plot(NDVI_2017,col=cl)
+plot(NDVI_2017,col=cl) #########################################MI VIENE SBAGLIATO L'INTERVALLO
 
 NDVI_2019<-(a19[[4]]-a19[[1]])/(a19[[4]]+a19[[1]])
 plot(NDVI_2019,col=cl)
@@ -121,73 +106,21 @@ plot(NDVI_2024,col=cl)
 #il range non è in funzione della radiazione radiometrica ma è un valore adimensionale che va da -1 a 1.
 
 dev.off()
-#-----------------------
-# Calcolo la differenza in termini di NDVI tra 2017 e 2019 (??????????????????????????):
-NDVI_diff1<-NDVI_2017-NDVI_2019
-plot(NDVI_diff1, col=cl)
+#-----------------------???????????
+#Creo uno stack con 
+NDVI_stack<-c(NDVI_2017, NDVI_2019, NDVI_2024)
+plot(NDVI_stack, col=cl)
+names(NDVI_stack)<-c("NDVI.2015","NDVI.2023")
+dev.off()
 
-# E tra 2019 e 2024:
-NDVI_diff2<-NDVI_2019-NDVI_2024
-plot(NDVI_diff2, col=cl)
+# Calcolo la DIFFERENZA in termini di NDVI tra 2017 e 2024:
+NDVI_diff<-NDVI_2017-NDVI_2024
+plot(NDVI_diff, col=cdiff)
 
-##### CLASSIFICAZIONE in 2 classi:
-#___________________________________________________________________________________???????????????????????????????
-NDVI_class1<-im.classify(NDVI_diff1,num_clusters = 2)
-plot(NDVI_class1)
-
-NDVI_class2<-im.classify(NDVI_diff2, 2)
-plot(NDVI_class2)
-
-par(mfrow=c(1,2))
-plot(NDVI_class1)
-plot(NDVI_class2)
-#____________________________??????????????????????????
-##### Classifico in 2 classi (è giusto il numero di 2 classi o sarebbe meglio 3????): 
-#classe 1 = foresta abbattuta da Vaia o colpita da bostrico, insiedamenti urbani
-#classe 2 = foresta sana e rigogliosa
-
-class17 <- im.classify(a17,num_clusters = 2)
-plot(class17)
-
-class19 <- im.classify(a19,num_clusters = 2)
-plot(class19)
-
-class24 <- im.classify(a24,num_clusters = 2)
-plot(class24)
-
-#Ora vogliamo quantificare le FREQUENZE, ossia il numero di pixel in ogni classe: 
-f17<-freq(class17) #se richiamo l'oggetto, vedrò la quantità di pixel per ogni classe
-
-#Ora facciamo una proporzione tra il numero di pixel di una classe (CHE CLASSE??????) e il totale.
-tot17<-ncell(class17) #per conoscere il totale dei pixel:
-prop17 = f17 / tot17 #proporzione
-perc17 = prop17 * 100 #percentuali: classe 1 = 60.57%  classe 2 = 39.43%
-
-f19 <- freq(class19) 
-tot19<-ncell(class19) #per conoscere il totale dei pixel:
-prop19 = f19 / tot19 #proporzione
-perc19 = prop19 * 100 #percentuali: classe 1 = 63.19%  classe 2 = 36.81%
-
-f24 <- freq(class24) 
-tot24<-ncell(class24) #per conoscere il totale dei pixel:
-prop24 = f24 / tot24 #proporzione
-perc24 = prop24 * 100 #percentuali: classe 1 = 56.15%  classe 2 = 43.85%
-
-#########non sono per niente convinta
-########quindi
-### ##### Classifico in 3 classi: 
-#classe 1 =
-#classe 2 =
-#classe 3 =
-
-class_17 <- im.classify(a17,num_clusters = 3)
-plot(class_17)
-
-class_19 <- im.classify(a19,num_clusters = 3)
-plot(class_19)
-
-class_24 <- im.classify(a24,num_clusters = 3)
-plot(class_24)
+#ora classifico per vedere dove è cambiato e dove no
+NDVI_class<-im.classify(NDVI_diff, 2) 
+plot(NDVI_class)
+dev.off()
 
 ######## Classifico con NDVI
 # classe 1 = uomo/neve/ghiaccio
@@ -223,39 +156,55 @@ tot_24<-ncell(c_ndvi24) #per conoscere il totale dei pixel:
 prop_24 = f_24 / tot_24 #proporzione
 perc_24 = prop_24 * 100 #percentuali: classe 1 = 7.5%  classe 2 = 70.6% classe 3 = 21.9%
 
-#Ottenuti i dati, costruiamo un DATASET, con la funzione data.frame che ci consente di creare delle tabelle:
-class <- c("bosco","uomo/neve/ghiaccio","no bosco")
-y2017 <- c(76.1,9,14.9)
-y2019 <- c(75.3,6.3,18.4)
-y2024 <- c(70.6,7.5,21.9)
-
-tabout <- data.frame(class, y2017, y2019, y2024)
-
-#Per vedere la tabella (attenzione che è case sensitive!):
-View(tabout)
-
-#Realizzo i grafici per i singoli anni:
-ggplot(tabout, aes(x=class, y=y2017, color=class)) + geom_bar(stat="identity",fill="white")
-ggplot(tabout, aes(x=class, y=y2019, color=class)) + geom_bar(stat="identity",fill="white")
-ggplot(tabout, aes(x=class, y=y2024, color=class)) + geom_bar(stat="identity",fill="white")
-
-#Per aggiustare questo, diamo un intervallo di valori per la y con la funzione ylim:
-p1<-ggplot(tabout, aes(x=class, y=y2017, color=class)) + geom_bar(stat="identity",fill="white") + ylim(c(0,100))
-p2<-ggplot(tabout, aes(x=class, y=y2019, color=class)) + geom_bar(stat="identity",fill="white") + ylim(c(0,100))
-p3<-ggplot(tabout, aes(x=class, y=y2024, color=class)) + geom_bar(stat="identity",fill="white") + ylim(c(0,100))
-
-p1 + p2 + p3 #Per vedere il confronto
-#####################################################
-##Altro DATASET:
+##Creo un DATASET con le percentuali ottenute:
 anno <- c("2017","2019","2024")
 bosco <- c(76.1,75.3,70.6)
 no_bosco <- c(14.9,18.4,21.9)
 altro <- c(9,6.3,7.5)
 
-tabout2 <- data.frame(anno, bosco, no_bosco, altro)
+tabout <- data.frame(anno, bosco, no_bosco, altro)
 
+# Creo dei GRAFICI:
 p1<-ggplot(tabout2, aes(x=anno, y=bosco, color=class)) + geom_bar(stat="identity",fill="white") + ylim(c(0,100))
 p2<-ggplot(tabout2, aes(x=anno, y=no_bosco, color=class)) + geom_bar(stat="identity",fill="white") + ylim(c(0,100))
 p3<-ggplot(tabout2, aes(x=anno, y=altro, color=class)) + geom_bar(stat="identity",fill="white") + ylim(c(0,100))
 
+p1 + p2 + p3 #Per vedere il confronto
 
+# MISURA della VARIABILITA':
+#------------2017
+nir <- a17[[4]]
+plot(nir)
+
+#Useremo la nuova funzione "focal" che permette di estarre valori focali, ossia statistiche, in un gruppo di valori: qui la dev standard.
+focal(nir, matrix (1/9, 3, 3), fun=sd)
+# nir è l'immagine usata
+# matrix: è una matrice ossia il secondo argomento della funzione focal(). 1/9 perchè useremo un unico pixel su 9, la matrice è di 3 x 3 pixel. È l'unità di misura. 
+# numero di pixel per colonna e per riga;
+# funzione usata è "standard deviation" N.B. non nominare con sd perchè corrisponde al nome della funzione
+
+#Rinominiamo l'oggetto:
+sd2017 <- focal(nir, matrix (1/9, 3, 3), fun=sd)
+plot(sd2017)
+
+#-----------2019
+nir2 <- a19[[4]]
+plot(nir)
+focal(nir2, matrix (1/9, 3, 3), fun=sd)
+#Rinominiamo l'oggetto:
+sd2019 <- focal(nir2, matrix (1/9, 3, 3), fun=sd)
+plot(sd2019)
+
+#------------2024
+nir3 <- a24[[4]]
+plot(nir3)
+focal(nir3, matrix (1/9, 3, 3), fun=sd)
+#Rinominiamo l'oggetto:
+sd2024 <- focal(nir3, matrix (1/9, 3, 3), fun=sd)
+plot(sd2024)
+
+#Ora creaiamo un mutliframe:
+par(mfrow=c(1,3))
+plot(sd2017,col=cl)
+plot(sd2019,col=cl)
+plot(sd2024,col=cl)
